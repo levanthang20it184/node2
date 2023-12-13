51f8f55ee12be2143d0b0e338ebd1040ca98d2fa
levanthang20it184 <89498938+levanthang20it184@users.noreply.github.com>
2023-12-14 02:28:29 +0700

    LeVanThang

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 // Cấu hình views
+app.use(express.static(__dirname + "/public/"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
